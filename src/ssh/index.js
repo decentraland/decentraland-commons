ef34f9bf41f20c9f9fefec9a040490b6ccb3153b
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-10-13 15:37:49 +0200

    Add ssh tunnel

---
+++ src/ssh/index.js
@@ -0,0 +1,76 @@
+import fs from 'fs'
+import sshTunnel from 'tunnel-ssh'
+
+import { Log } from '../utils'
+import { sleep } from '../utils'
+
+const log = new Log('[SSH]')
+
+
+/**
+ * Connect via an SSH tunnel. It uses tunnel-ssh behind the scenes {@link https://github.com/agebrock/tunnel-ssh}
+ * @namespace
+ */
+const tunnel = {
+  /**
+   * Connect using an SSH tunnel
+   * @param  {object|string} configOrPath - Object or path to JSON file which describes the connection. @see readTunnelConfig
+   * @return {Promise<number>} - Fires when the tunnel connection has been stablished. It receives the tunnel port as a parameter
+   */
+  connect: function(configOrPath) {
+    let tunnelConfig = this.readTunnelConfig(configOrPath)
+
+    if (! tunnelConfig) throw new Error('Tried to connect to ssh tunnel without a valid configuration')
+
+    log.info('Connecting to SSH tunnel')
+    let currentTunnel = null
+
+    return new Promise((resolve, reject) =>
+      sshTunnel(tunnelConfig, (error, _tunnel) => {
+        currentTunnel = _tunnel
+        resolve(tunnelConfig.localPort)
+
+      }).on('error', async (error, tunnel) => {
+        log.info('Tunnel error', error.toString())
+        if (currentTunnel) currentTunnel.close()
+        this.close()
+
+        await sleep(1000)
+        tunnel.connect(configOrPath).then(resolve).catch(reject)
+      })
+    )
+  },
+
+  /**
+   * Uses an object or reads one from the file system describing the connection. A minimal example contains:
+   * {
+   *   "username": "remote_username",
+   *   "host": "remote_host",
+   *   "privateKey": "path_to_private_key",
+   *   "port": 22024,
+   *   "dstPort": 27017,
+   *   "localPort": ""
+   * }
+   * @param  {object|string} configOrPath [description]
+   * @return {[type]}            [description]
+   */
+  readTunnelConfig(configOrPath) {
+    let tunnelConfig = null
+
+    if (typeof configOrPath === 'string') {
+      tunnelConfig = fs.readFileSync(configOrPath)
+      tunnelConfig = JSON.parse(tunnelConfig)
+
+    } else {
+      tunnelConfig = Object.assign({}, configOrPath)
+    }
+
+    if (tunnelConfig && tunnelConfig.privateKey) {
+      tunnelConfig.privateKey = fs.readFileSync(tunnelConfig.privateKey)
+    }
+
+    return tunnelConfig
+  }
+}
+
+export default tunnel
