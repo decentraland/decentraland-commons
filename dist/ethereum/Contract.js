288cc6bda7a6e5e68e274b25f4989932dde9ae06
Esteban Ordano <esteban@decentraland.org>
2017-11-20 14:08:12 -0300

    Version 1.3.1

--- dist/ethereum/Contract.js
+++ dist/ethereum/Contract.js
@@ -2,7 +2,7 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _abiDecoder = require('abi-decoder');
+var _abiDecoder = require('./abi-decoder');
 
 var _abiDecoder2 = _interopRequireDefault(_abiDecoder);
 
