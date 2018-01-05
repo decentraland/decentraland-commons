359a194bca216bf346367316b2698ecfa5a6dc7e
Esteban Ordano <esteban@decentraland.org>
2018-01-05 03:41:19 -0300

    lint fixes

--- src/ethereum/ReturnMANA.js
+++ src/ethereum/ReturnMANA.js
@@ -31,23 +31,49 @@ class ReturnMANA extends Contract {
 
   burn(amount, opts = {}) {
     log.info(`(burn) ${amount} MANA`)
-    return this.transaction('burn', amount, Object.assign({}, {
-      gas: 120000
-    }, opts))
+    return this.transaction(
+      'burn',
+      amount,
+      Object.assign(
+        {},
+        {
+          gas: 120000
+        },
+        opts
+      )
+    )
   }
 
   transferBackMANA(address, amount, opts = {}) {
     log.info(`(transferBackMANA) ${amount} to ${address}`)
-    return this.transaction('transferBackMANA', address, amount, Object.assign({}, {
-      gas: 120000
-    }, opts))
+    return this.transaction(
+      'transferBackMANA',
+      address,
+      amount,
+      Object.assign(
+        {},
+        {
+          gas: 120000
+        },
+        opts
+      )
+    )
   }
 
   transferBackMANAMany(addresses, amounts, opts = {}) {
     log.info(`(transferBackMANAMany) ${amounts} to ${addresses}`)
-    return this.transaction('transferBackMANAMany', addresses, amounts, Object.assign({}, {
-      gas: 120000
-    }, opts))
+    return this.transaction(
+      'transferBackMANAMany',
+      addresses,
+      amounts,
+      Object.assign(
+        {},
+        {
+          gas: 120000
+        },
+        opts
+      )
+    )
   }
 }
 
