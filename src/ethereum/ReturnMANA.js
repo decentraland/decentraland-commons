f7a9947fb48fefb2f9ea1fff90b7e4de93a4b08c
Esteban Ordano <esteban@decentraland.org>
2018-01-05 03:40:43 -0300

    add getBlock, assign opts

--- src/ethereum/ReturnMANA.js
+++ src/ethereum/ReturnMANA.js
@@ -29,25 +29,25 @@ class ReturnMANA extends Contract {
     return instance
   }
 
-  burn(amount) {
+  burn(amount, opts = {}) {
     log.info(`(burn) ${amount} MANA`)
-    return this.transaction('burn', amount, {
+    return this.transaction('burn', amount, Object.assign({}, {
       gas: 120000
-    })
+    }, opts))
   }
 
-  transferBackMANA(address, amount) {
+  transferBackMANA(address, amount, opts = {}) {
     log.info(`(transferBackMANA) ${amount} to ${address}`)
-    return this.transaction('transferBackMANA', address, amount, {
+    return this.transaction('transferBackMANA', address, amount, Object.assign({}, {
       gas: 120000
-    })
+    }, opts))
   }
 
-  transferBackMANAMany(addresses, amounts) {
+  transferBackMANAMany(addresses, amounts, opts = {}) {
     log.info(`(transferBackMANAMany) ${amounts} to ${addresses}`)
-    return this.transaction('transferBackMANAMany', addresses, amounts, {
+    return this.transaction('transferBackMANAMany', addresses, amounts, Object.assign({}, {
       gas: 120000
-    })
+    }, opts))
   }
 }
 
