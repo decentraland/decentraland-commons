49c0087ecb7470a910d467f151c6e7b1623428d5
decentraland_bot <hello@decentraland.org>
2018-02-27 00:03:08 +0000

    Deploy to GitHub pages [ci skip]

--- src/db/index.js
+++
@@ -1,2 +0,0 @@
-export { postgres } from './postgres'
-export { mongo } from './mongo'
--- src/ethereum/index.js
+++
@@ -1,5 +0,0 @@
-export { eth } from './eth'
-export { Contract } from './Contract'
-export { Event } from './Event'
-export { SignedMessage } from './SignedMessage'
-export { txUtils } from './txUtils'
