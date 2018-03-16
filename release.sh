9b19085a01434e4e097d8099dfc1cda6392b8daa
Esteban Ordano <esteban@decentraland.org>
2018-03-16 18:04:36 -0300

    chore: add -f to release

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ set -e
 git checkout release
 git merge master
 npm run build
-git add dist
+git add -f dist
 git status
 echo -e "\n-------------------------------------------------"
 echo -e "Rebased branch ready to be committed and pushed\n"
