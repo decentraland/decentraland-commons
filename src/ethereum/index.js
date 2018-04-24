09305cbe34359584cd52a9b593dd587162887bf3
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-04-24 16:33:53 +0200

    chore: remove ethereum code (#66)
    BREAKING CHANGE: ethereum related code is no longer exported from this lib

--- src/ethereum/index.js
+++
@@ -1,9 +0,0 @@
-import * as wallets from './wallets'
-
-export { eth } from './eth'
-export { Contract } from './Contract'
-export { Abi } from './abi'
-export { Event } from './Event'
-export { SignedMessage } from './SignedMessage'
-export { txUtils } from './txUtils'
-export { wallets }
